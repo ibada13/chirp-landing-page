c7572ffcc3d0c982ab107cde001f6d96bd131a68
hunter666hz <hzhunter7@gmail.com>
2024-10-09 17:55:58 +0100

    modified:   app/chirp-landing-page/components/Features.tsx

--- app/chirp-landing-page/components/Features.tsx
+++ app/chirp-landing-page/components/Features.tsx
@@ -1,9 +1,12 @@
 import {features} from './lib/data'
 export default function Features() { 
     return (
-        <div>
-
-        <div className="h-auto grid  lg:grid-cols-2 gap-6 w-4/5">
+        <div className='flex flex-col w-4/5 items-start gap-y-4'>
+            <div className='w-2/5 bg-red-400 mt-6 mb-6'>
+                
+            <p className='text-2xl font-bold'>Features that help you Tweet smarter.</p>
+            </div>
+        <div className="h-auto grid  lg:grid-cols-2 gap-6 ">
             {features.map((e, i) => (
                 
                 <div key={`f-${i}`} className='flex flex-col gap-2'>
