1028e0c0599c364574444203988a6e87c8e71708
hunter666hz <hzhunter7@gmail.com>
2024-10-09 17:28:33 +0100

    modified:   app/chirp-landing-page/components/lib/data.ts

--- app/chirp-landing-page/components/lib/data.ts
+++ app/chirp-landing-page/components/lib/data.ts
@@ -1,21 +1,27 @@
-
+import Image from "next/image"
+import chart from '../Assets/chart.svg'
 
 interface feature {
+    emoji:String,
     title: string,
     text:string
 }
 const features: feature[] = [
     {
+        emoji:'📈',
         title:"title 1",
         text:"somt text Lorem ipsum odor amet, consectetuer adipiscing elit. Ad bibendum hendrerit faucibus himenaeos amet montes hac mauris dolor elit erat proin venenatis potenti eu ut viverra nunc malesuada fringilla ultricies porttitor.",
     }
-    ,    {
+    , {
+        emoji:"🤓",
         title:"title 2 ",
         text:"Lorem ipsum odor amet, consectetuer adipiscing elit. Ad bibendum hendrerit faucibus himenaeos amet montes hac mauris dolor elit erat proin venenatis potenti eu ut viverra nunc malesuada fringilla ultricies porttitor.",
     },    {
+        emoji:"📝",
         title:"title 3",
         text:"Lorem ipsum odor amet, consectetuer adipiscing elit. Ad bibendum hendrerit faucibus himenaeos amet montes hac mauris dolor elit erat proin venenatis potenti eu ut viverra nunc malesuada fringilla ultricies porttitor.",
-    },    {
+    }, {
+        emoji:"🌑",
         title:"title 4",
         text:"Lorem ipsum odor amet, consectetuer adipiscing elit. Ad bibendum hendrerit faucibus himenaeos amet montes hac mauris dolor elit erat proin venenatis potenti eu ut viverra nunc malesuada fringilla ultricies porttitor.",
     }
